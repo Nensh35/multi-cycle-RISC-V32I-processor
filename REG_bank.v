7c242ffbebfdc9b4e8862212a1cc0cf3057cecf8
Nensh35 <nenshmaheshwari@gmail.com>
2026-08-30 07:11:42 +0530

    Update REG_bank.v

--- REG_bank.v
+++ REG_bank.v
@@ -10,25 +10,35 @@ module registers(
     input [31:0] write_data,
     
     output [31:0] rs1_data, 
-    output [31:0] rs2_data  
+    output [31:0] rs2_data,reg_1_data  
 );
 
    
     reg [31:0] R [31:0];
     integer i;
 
-    // 1. COMBINATIONAL READS (Instantaneous!)
+    // 1. COMBINATIONAL READS (Instantaneous)
     // If address is 0, always output 0. Otherwise, output the register data.
+
+    assign reg_1_data = R[1] ;
 
     assign rs1_data = (rs1_addr == 5'b00000) ? 32'b0 : R[rs1_addr];
     assign rs2_data = (rs2_addr == 5'b00000) ? 32'b0 : R[rs2_addr];
 
     // 2. SYNCHRONOUS WRITES (Happens strictly on the clock tick)
 
-    always @(posedge clk) begin
+    always @(negedge clk) begin
         if (reset) begin
-            for (i = 0; i < 32; i = i + 1)
+        for (i = 0; i < 32; i = i + 1)
                 R[i] <= 32'b0;
+        
+/*    R[20] <= 32'd5;
+    R[2]  <= 32'd20;
+    R[3]  <= 32'd1;
+    R[1]  <= 32'd0;*/
+
+        
+           
         end 
         else if (RegWrite && rd_addr != 5'b00000) begin
         
